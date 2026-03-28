11679f377da885f9e846f63a86c5cb08518c78bf
Rishikesh More <rishikesh.more2@tatacommunications.com>
2026-03-28 15:51:53 +0530

    Initial Terraform setup

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  cloud {
+    organization = "TCLV2"
+
+    workspaces {
+      name = "TCLWS"
+    }
+  }
+
+  required_providers {
+    genesyscloud = {
+      source = "mypurecloud/genesyscloud"
+    }
+  }
+}
+
+provider "genesyscloud" {}
+
+resource "genesyscloud_routing_queue" "test_queue" {
+  name = "Test Queue from Terraform"
+}
